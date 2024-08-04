bb08f5d3b2b3710d257023c85cc51ee28f7b0ee5
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-08-04 16:46:53 +0200

    host n port

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+router.beforeEach(to => {
+    if (!to.matched.length) window.location = "/404"
+  })
+
+
+// app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
